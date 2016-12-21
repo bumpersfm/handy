73d71a4752ea0797ff863f785d26c1dc811cbd10
Dani Postigo <dani.postigo@gmail.com>
2016-12-21 14:20:22 -0500

    update to v0.1.8

--- Handy/UIButton.swift
+++ Handy/UIButton.swift
@@ -10,35 +10,67 @@ import Foundation
 import UIKit
 
 extension UIButton {
-    
-    public convenience init(title: String, color: UIColor? = nil, font: UIFont? = nil, backgroundColor: UIColor? = nil, contentEdgeInsets: UIEdgeInsets = UIEdgeInsetsZero) {
-        self.init()
-        self.setTitle(title, forState: .Normal)
-        self.setTitleColor(color, forState: .Normal)
+    public convenience init(title: String) {
+        self.init(); self.setTitle(title, forState: .Normal)
+    }
+    public convenience init(title: String, font: UIFont) {
+        self.init(title: title)
         self.titleLabel?.font = font
+    }
+    
+    public convenience init(title: String, font: UIFont, color: UIColor) {
+        self.init(title: title, font: font)
+        self.setTitleColor(color, forState: .Normal)
+    }
+    
+    public convenience init(title: String, backgroundColor: UIColor) {
+        self.init(title: title); self.backgroundColor = backgroundColor
+    }
+    
+    public convenience init(title: String, font: UIFont, color: UIColor, backgroundColor: UIColor) {
+        self.init(title: title, font: font, color: color)
         self.backgroundColor = backgroundColor
+    }
+    
+    public convenience init(title: String, font: UIFont, color: UIColor, contentEdgeInsets: UIEdgeInsets) {
+        self.init(title: title, font: font, color: color)
         self.contentEdgeInsets = contentEdgeInsets
     }
-
+    
     public convenience init(title: String, titleColor: UIColor) {
-        self.init(title: title, color: titleColor)
+        self.init(title: title); self.setTitleColor(titleColor, forState: .Normal)
     }
-
+    
     public convenience init(attributedTitle: NSAttributedString) {
-        self.init(); self.setAttributedTitle(attributedTitle, forState: .Normal)
+        self.init()
+        self.setAttributedTitle(attributedTitle, forState: .Normal)
     }
-
-    public convenience init(image: UIImage?, selectedImage: UIImage? = nil, contentEdgeInsets: UIEdgeInsets = UIEdgeInsetsZero) {
+    
+    public convenience init(image: UIImage?) {
         self.init()
         self.setImage(image, forState: .Normal)
-        self.setImage(selectedImage, forState: .Selected)
+    }
+    
+    public convenience init(image: UIImage?, selectedImage: UIImage?) {
+        self.init(image: image); self.setImage(selectedImage, forState: .Selected)
+    }
+    
+    public convenience init(image: UIImage?, contentEdgeInsets: UIEdgeInsets) {
+        self.init(image: image)
         self.contentEdgeInsets = contentEdgeInsets
     }
-
+    
     public convenience init(userInteractionEnabled: Bool) {
-        self.init(); self.userInteractionEnabled = userInteractionEnabled
+        self.init()
+        self.userInteractionEnabled = userInteractionEnabled
     }
-
+    
+    // MARK:
+    
+    public convenience init(title: String, color: UIColor) {
+        self.init(title: title, titleColor: color)
+    }
+    
     // MARK: Attributed strings
     
     public var attributes: [String: AnyObject]? {
